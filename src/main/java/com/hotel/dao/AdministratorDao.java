38da2bc548764f2ab39f00c0e47aa5cd5e616864
MysticalGuest <1317148109@qq.com>
2020-09-15 22:33:49 +0800

    Update AdministratorDao.java

--- src/main/java/com/hotel/dao/AdministratorDao.java
+++ src/main/java/com/hotel/dao/AdministratorDao.java
@@ -2,16 +2,13 @@ package com.hotel.dao;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 import com.hotel.entity.Administrator;
 
 @Mapper // 标记当前类为功能映射文件
 public interface AdministratorDao {
+	
 	@Insert("insert into administrator (AdmId,aName,aPassword,aSex,limit) values (#{AdmId},#{aName},#{aPassword},#{aSex},#{limit})")
 	@Options(useGeneratedKeys = true, keyProperty = "AdmId")
 	int insert(Administrator administrator);
